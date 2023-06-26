62e7aae0c32200243d748073c01723139085cb23
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2023-06-26 23:30:55 +0300

    Remove unneeded comments

--- BookingSystem/DataLayer/DataLayer.cpp
+++ BookingSystem/DataLayer/DataLayer.cpp
@@ -1,58 +1,6 @@
 #include "pch.h"
 #include "DataLayer.hpp"
 
-//void User::insert(int id, const std::string& name, int age, const std::string& email)
-//{
-//    // Define the SQL statement
-//    std::string sql = "INSERT INTO users (id, name, age, email) VALUES (?, ?, ?, ?)";
-//
-//    // Prepare the SQL statement
-//    nanodbc::statement stmt(conn_);
-//    stmt.prepare(sql);
-//
-//    // Bind the parameters to the statement
-//    stmt.bind(0, &id);
-//    stmt.bind(1, name.c_str());
-//    stmt.bind(2, &age);
-//    stmt.bind(3, email.c_str());
-//
-//    // Execute the statement
-//    stmt.execute();
-//}
-//
-//void User::update(int id, const std::string& name, int age, const std::string& email)
-//{
-//    // Define the SQL statement
-//    std::string sql = "UPDATE users SET name = ?, age = ?, email = ? WHERE id = ?";
-//
-//    // Prepare the SQL statement
-//    nanodbc::statement stmt(conn_);
-//    stmt.prepare(sql);
-//
-//    // Bind the parameters to the statement
-//    stmt.bind(0, name.c_str());
-//    stmt.bind(1, &age);
-//    stmt.bind(2, email.c_str());
-//    stmt.bind(3, &id);
-//
-//    // Execute the statement
-//    stmt.execute();
-//}
-//void User::remove(int id)
-//{
-//    // Define the SQL statement
-//    std::string sql = "DELETE FROM users WHERE id = ?";
-//
-//    // Prepare the SQL statement
-//    nanodbc::statement stmt(conn_);
-//    stmt.prepare(sql);
-//
-//    // Bind the parameter to the statement
-//    stmt.bind(0, &id);
-//
-//    // Execute the statement
-//    stmt.execute();
-//}
 Data* Data::instance = nullptr;
 Data* Data::getInstance()
 {
--- BookingSystem/DataLayer/DataLayer.hpp
+++ BookingSystem/DataLayer/DataLayer.hpp
@@ -5,16 +5,6 @@ class Data
 {
 public:
 
-    //// Define a function to insert a user into the database
-    //void insert(int id, const std::string& name, int age, const std::string& email);
-
-    //// Define a function to update a user in the database
-
-    //void update(int id, const std::string& name, int age, const std::string& email);
-
-    //// Define a function to delete a user from the database
-    //void remove(int id);
-
     // Define a function to select a user from the database
     bool select(std::string username, std::string password);
     ~Data();
